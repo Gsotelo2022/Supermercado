51ac7d1c93613086b21ef483414f6ee228b4edae
Gsotelo2022 <100044399+Gsotelo2022@users.noreply.github.com>
2023-05-16 20:12:50 -0300

    Se crea: Carga_Stock Producto N_Carga_Stock N_Producto

---
+++ Entidades/Producto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+		private int _id_Prodcuto;
+
+		public int Id_Producto
+		{
+			get { return _id_Prodcuto; }
+			set { _id_Prodcuto = value; }
+		}
+		private string _descripcion;
+
+		public string Descripcion
+		{
+			get { return _descripcion; }
+			set { _descripcion = value; }
+		}
+
+		private double _precio;
+
+		public double Precio
+		{
+			get { return _precio; }
+			set { _precio = value; }
+		}
+		private int _cantidad;
+
+		public int Cantidad
+		{
+			get { return _cantidad; }
+			set { _cantidad = value; }
+		}
+
+		private bool _habilitado;
+
+		public bool Habilitado
+		{
+			get { return _habilitado; }
+			set { _habilitado = value; }
+		}		
+	}
+}
---
+++ Negocio/N_Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class N_Producto
+    {
+        /*Metodo para actualizar cantidad*/
+        public void actualizarCantidad() {
+            N_Carga_Stock objCarga = new N_Carga_Stock();
+			objCarga.cargarStock();
+		}
+    }
+}
