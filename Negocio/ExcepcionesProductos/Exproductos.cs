6c25e623028c9814d9086412830d87ef76f3e0e8
Gsotelo2022 <100044399+Gsotelo2022@users.noreply.github.com>
2023-05-27 22:03:39 -0300

    Se agregan las excepciones para la creacion del producto

---
+++ Negocio/ExcepcionesProductos/Exproductos.cs
@@ -0,0 +1,39 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.ExcepcionesProductos
+{
+    public class Exproductos
+    {
+        public static void verificarCampos(string _descripcion, double _precio, int _cantidad, string _habilitado)
+        {
+            //Descripcion
+            if (string.IsNullOrEmpty(_descripcion))
+            {
+                throw new Exception("No ingresó Descripción del producto");
+            }
+                       //Precio
+            if (_precio == 0)
+            {
+                throw new Exception("El precio del producto no es válido");
+            }
+            //Cantidad
+            if (_cantidad == 0)
+            {
+                throw new Exception("La cantidad del producto no es válida");
+            }
+            //Descripcion
+            if (string.IsNullOrEmpty(_habilitado))
+            {
+                throw new Exception("No definio si el producto esta Habilitado o no");
+            }            
+        }
+
+        public void Exproducto() { 
+        }
+    }  
+}
