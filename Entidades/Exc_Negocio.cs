bd4c508d4847f53120f2cdbff0d613a0ee7781a2
Yaggi <yaggim@gmail.com>
2023-06-09 16:57:00 -0300

    Se vuelve atras el cambio

---
+++ Entidades/Exc_Negocio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Exc_Negocio : Exception
+    {
+        #region ATRIBUTOS
+
+        private string mensaje;
+
+        #endregion
+
+        /// <summary>
+        /// Constructor: Instancia el valor de la variabel local de la clase.
+        /// </summary>
+        /// <param name="texto"></param>
+        public Exc_Negocio(string texto)
+        {
+            this.Mensaje = texto;
+        }
+
+        #region PROPIEDADES
+
+        public string Mensaje
+        {
+            get { return mensaje; }
+            set { mensaje = value; }
+        }
+
+        #endregion
+    }
+}
