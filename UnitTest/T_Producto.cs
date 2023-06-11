92d6f6a71cdd043730ce2da8de55b9eeeb295f00
Delfina <gomezdelfina25@gmail.com>
2023-06-11 19:56:07 -0300

    TestProject

---
+++ UnitTest/T_Producto.cs
@@ -0,0 +1,30 @@
+﻿using Datos;
+using Entidades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class T_Producto
+    {
+        [TestMethod]
+        public void BuscarProductoPorId()
+        {
+            D_Producto objDProducto = new D_Producto();
+            E_Producto objEProducto = new E_Producto();
+
+            int idProducto = 5; 
+
+            DataTable dtProducto = objDProducto.BuscarProductoPorId(idProducto);
+
+            bool resultado = dtProducto.Rows.Count == 0 ? false : true;
+
+            Assert.IsTrue(resultado);
+        }
+    }
+}
